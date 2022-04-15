8567ab11e64754e4c2ae59b135f7a15dd3aee4d7
Jean-Francois Numainville <jfnumainville@gmail.com>
2022-04-14 20:56:14 -0400

    Unfinished simple AirBNB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Flat.destroy_all
+puts "Creating Flats..."
+
+5.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.street_address
+  description = Faker::Restaurant.description
+  price_per_night = rand(100..400)
+  number_of_guests = rand(2..6)
+  picture_url = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg"].sample(1)
+  p Flat.create!(name: name, address: address, description:description, price_per_night: price_per_night, number_of_guests: number_of_guests, picture_url: picture_url[0])
+end
